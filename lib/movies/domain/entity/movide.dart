2b504a1a6b0e6a669f5ee56507e6852dd96a58cb
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-07 13:15:51 +0300

    Update movide.dart

--- lib/movies/domain/entity/movide.dart
+++ lib/movies/domain/entity/movide.dart
@@ -1,8 +1,10 @@
-class Movie {
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
   final int id;
   final String title;
   final String backdropBath;
-  final List<int> genderIds;
+  final List<int> genreIds;
   final String overview;
   final double voteAverage;
 
@@ -10,29 +12,18 @@ class Movie {
     required this.id,
     required this.title,
     required this.backdropBath,
-    required this.genderIds,
+    required this.genreIds,
     required this.overview,
     required this.voteAverage,
   });
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Movie &&
-          runtimeType == other.runtimeType &&
-          id == other.id &&
-          title == other.title &&
-          backdropBath == other.backdropBath &&
-          genderIds == other.genderIds &&
-          overview == other.overview &&
-          voteAverage == other.voteAverage;
-
-  @override
-  int get hashCode =>
-      id.hashCode ^
-      title.hashCode ^
-      backdropBath.hashCode ^
-      genderIds.hashCode ^
-      overview.hashCode ^
-      voteAverage.hashCode;
+  List<Object?> get props => [
+        id,
+        title,
+        backdropBath,
+        genreIds,
+        overview,
+        voteAverage,
+      ];
 }
