04824c9b49189b9bc0ac0c80cb3b1a2c6959b6ba
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 11:58:30 +0300

    Create movies_repository.dart

---
+++ lib/movies/data/repository/movies_repository.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:popcorn/core/error/exceptions.dart';
+import 'package:popcorn/core/error/failure.dart';
+import 'package:popcorn/movies/data/datasource/movie_remote_data_source.dart';
+import 'package:popcorn/movies/domain/entity/movie.dart';
+import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
+
+class MoviesRepository extends BaseMoviesRepository {
+  final BaseMoviesRemoteDataSource baseMovieRemoteDataSource;
+
+  MoviesRepository({required this.baseMovieRemoteDataSource});
+
+  @override
+  Future<Either<Failure, List<Movie>>> getNowPlayingMovies() async {
+    final result = await baseMovieRemoteDataSource.getNowPlayingMovies();
+
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(
+          ServerFailure(message: failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Movie>>> getPopularMovies() async {
+    final result = await baseMovieRemoteDataSource.getPopularMovies();
+
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(
+          ServerFailure(message: failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies() async {
+    final result = await baseMovieRemoteDataSource.getTopRatedMovies();
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(
+          ServerFailure(message: failure.errorMessageModel.statusMessage));
+    }
+  }
+}
