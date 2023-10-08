ebbb6cdb705da6a6712ed53b04e177e62c4a7e7a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 11:58:26 +0300

    Update movie_remote_data_source.dart

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -1,12 +1,47 @@
 import 'package:dio/dio.dart';
 import 'package:popcorn/core/error/exceptions.dart';
+import 'package:popcorn/core/network/api_constants.dart';
 import 'package:popcorn/core/network/error_message_model.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
 
-class MovieRemoteDataSource {
+abstract class BaseMoviesRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovies(); //? this is called contract
+
+  Future<List<MovieModel>> getPopularMovies();
+
+  Future<List<MovieModel>> getTopRatedMovies();
+}
+
+class MoviesRemoteDataSource extends BaseMoviesRemoteDataSource {
+  @override
   Future<List<MovieModel>> getNowPlayingMovies() async {
-    final response = await Dio().get(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=944496b8feb259fafe467ef6f4d38c80');
+    final response = await Dio().get(ApiConstants.nowPlayingMoviesUrl);
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data['results'] as List)
+          .map((e) => MovieModel.fromJson(e)));
+    } else {
+      throw ServerException(
+          errorMessageModel: ErrorMessageModel.fromJson(response.data));
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() async {
+    final response = await Dio().get(ApiConstants.popularMoviesUrl);
+
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data['results'] as List)
+          .map((e) => MovieModel.fromJson(e)));
+    } else {
+      throw ServerException(
+          errorMessageModel: ErrorMessageModel.fromJson(response.data));
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getTopRatedMovies() async {
+    final response = await Dio().get(ApiConstants.topRatedMoviesUrl);
 
     if (response.statusCode == 200) {
       return List<MovieModel>.from((response.data['results'] as List)
