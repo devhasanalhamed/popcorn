8b810a87089fbd453e35cf51bed696162e57e40c
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 07:00:06 +0300

    Create exceptions.dart

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,11 @@
+import 'package:popcorn/core/network/error_message_model.dart';
+
+class ServerException implements Exception {
+  final ErrorMessageModel errorMessageModel;
+
+  ServerException({
+    required this.errorMessageModel,
+  });
+
+
+}
