778826226c9f5419e1dff398da69f81c9aa25b56
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 07:00:11 +0300

    Create error_message_model.dart

---
+++ lib/core/network/error_message_model.dart
@@ -0,0 +1,18 @@
+class ErrorMessageModel {
+  final int statusCode;
+  final String statusMessage;
+  final bool success;
+
+  ErrorMessageModel({
+    required this.statusCode,
+    required this.statusMessage,
+    required this.success,
+  });
+
+  factory ErrorMessageModel.fromJson(Map<String, dynamic> json) =>
+      ErrorMessageModel(
+        statusCode: json['status_code'],
+        statusMessage: json['status_message'],
+        success: json['success'],
+      );
+}
