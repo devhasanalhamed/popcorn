5fe0658ab24f2a7df5990c1bfa56fed205091687
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 13:23:52 +0300

    Update base_movies_repository.dart

--- lib/movies/domain/repository/base_movies_repository.dart
+++ lib/movies/domain/repository/base_movies_repository.dart
@@ -1,12 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:popcorn/core/error/failure.dart';
-import 'package:popcorn/movies/domain/entity/movie.dart';
+import 'package:popcorn/movies/data/models/movie_model.dart';
 
 //! this is called contract
 abstract class BaseMoviesRepository {
-  Future<Either<Failure, List<Movie>>> getNowPlayingMovies();
+  Future<Either<Failure, List<MovieModel>>> getNowPlayingMovies();
 
-  Future<Either<Failure, List<Movie>>> getPopularMovies();
+  Future<Either<Failure, List<MovieModel>>> getPopularMovies();
 
-  Future<Either<Failure, List<Movie>>> getTopRatedMovies();
+  Future<Either<Failure, List<MovieModel>>> getTopRatedMovies();
 }
