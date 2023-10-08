247f417a681f474afe5053fce56cd845b9318f0e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-08 13:24:05 +0300

    Update movies_screen.dart

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -1,10 +1,24 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:popcorn/movies/presentation/controller/movies_bloc.dart';
+import 'package:popcorn/movies/presentation/controller/movies_event.dart';
+import 'package:popcorn/movies/presentation/controller/movies_state.dart';
 
 class MoviesScreen extends StatelessWidget {
   const MoviesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return BlocProvider(
+      create: (context) => MoviesBloc()..add(GetNowPlayingMoviesEvent()),
+      child: BlocBuilder<MoviesBloc, MoviesState>(
+        builder: (context, state) {
+          log('$state');
+          return const Scaffold();
+        },
+      ),
+    );
   }
 }
