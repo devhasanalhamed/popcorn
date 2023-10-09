4fb7660ae2dc9a727abef742e1ceae11bfccd833
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-09 07:04:29 +0300

    Update movies_bloc.dart

--- lib/movies/presentation/controller/movies_bloc.dart
+++ lib/movies/presentation/controller/movies_bloc.dart
@@ -1,22 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:popcorn/core/utils/enums.dart';
-import 'package:popcorn/movies/data/datasource/movie_remote_data_source.dart';
-import 'package:popcorn/movies/data/repository/movies_repository.dart';
-import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
 import 'package:popcorn/movies/domain/usecase/get_now_playing_movies_usecase.dart';
 import 'package:popcorn/movies/presentation/controller/movies_event.dart';
 import 'package:popcorn/movies/presentation/controller/movies_state.dart';
 
 class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
-  MoviesBloc() : super(const MoviesState()) {
+  final GetNowPlayingMoviesUseCase getNowPlayingMoviesUseCase;
+  MoviesBloc(
+    this.getNowPlayingMoviesUseCase,
+  ) : super(const MoviesState()) {
     on<GetNowPlayingMoviesEvent>((event, emit) async {
-      BaseMoviesRemoteDataSource baseMoviesRemoteDataSource =
-          MoviesRemoteDataSource();
-      BaseMoviesRepository baseMoviesRepository = MoviesRepository(
-          baseMovieRemoteDataSource: baseMoviesRemoteDataSource);
-      final result = await GetNowPlayingMoviesUseCase(
-              baseMoviesRepository: baseMoviesRepository)
-          .execute();
+      final result = await getNowPlayingMoviesUseCase.execute();
       emit(const MoviesState(nowPlayingMoviesState: RequestState.loaded));
       result.fold(
         (l) => emit(MoviesState(
