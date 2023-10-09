ac2688904c1d18edc58e19ef70094582c7af5a9e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-09 09:03:09 +0300

    Update movie_model.dart

--- lib/movies/data/models/movie_model.dart
+++ lib/movies/data/models/movie_model.dart
@@ -4,18 +4,20 @@ class MovieModel extends Movie {
   const MovieModel({
     required super.id,
     required super.title,
-    required super.backdropBath,
+    required super.backdropPath,
     required super.genreIds,
     required super.overview,
     required super.voteAverage,
+    required super.releaseDate,
   });
 
   factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
         id: json['id'],
         title: json['title'],
-        backdropBath: json['backdrop_path'],
+        backdropPath: json['backdrop_path'],
         genreIds: List<int>.from(json['genre_ids'].map((e) => e)),
         overview: json['overview'],
         voteAverage: json['vote_average'].toDouble(),
+        releaseDate: '',
       );
 }
