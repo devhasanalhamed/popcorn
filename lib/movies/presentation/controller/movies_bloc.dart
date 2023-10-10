ac5e77aa9e8128e5fe2febeffe15c030aaec2142
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-10 10:35:41 +0300

    Update movies_bloc.dart
    add two more use cases to the bloc and test poplar movies functionality

--- lib/movies/presentation/controller/movies_bloc.dart
+++ lib/movies/presentation/controller/movies_bloc.dart
@@ -1,27 +1,50 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:popcorn/core/utils/enums.dart';
 import 'package:popcorn/movies/domain/usecase/get_now_playing_movies_usecase.dart';
+import 'package:popcorn/movies/domain/usecase/get_popular_movies_usecase.dart';
+import 'package:popcorn/movies/domain/usecase/get_top_rated_movies_usecase.dart';
 import 'package:popcorn/movies/presentation/controller/movies_event.dart';
 import 'package:popcorn/movies/presentation/controller/movies_state.dart';
 
 class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
   final GetNowPlayingMoviesUseCase getNowPlayingMoviesUseCase;
+  final GetPopularMoviesUseCase getPopularMoviesUseCase;
+  final GetTopRatedMoviesUseCase getTopRatedMoviesUseCase;
   MoviesBloc(
     this.getNowPlayingMoviesUseCase,
+    this.getPopularMoviesUseCase,
+    this.getTopRatedMoviesUseCase,
   ) : super(const MoviesState()) {
     on<GetNowPlayingMoviesEvent>((event, emit) async {
       final result = await getNowPlayingMoviesUseCase.execute();
-      emit(const MoviesState(nowPlayingMoviesState: RequestState.loaded));
       result.fold(
         (l) => emit(MoviesState(
           nowPlayingMoviesState: RequestState.error,
           nowPlayingMessage: l.message,
         )),
         (r) => emit(MoviesState(
-          nowPlayingMoviesState: RequestState.loaded,
           nowPlayingMovies: r,
+          nowPlayingMoviesState: RequestState.loaded,
         )),
       );
     });
+    on<GetPopularMoviesEvent>((event, emit) async {
+      final result = await getPopularMoviesUseCase.execute();
+      result.fold(
+        (l) => emit(
+          MoviesState(
+            popularMoviesState: RequestState.error,
+            popularMessage: l.message,
+          ),
+        ),
+        (r) => emit(
+          MoviesState(
+            popularMovies: r,
+            popularMoviesState: RequestState.loaded,
+          ),
+        ),
+      );
+    });
+    on<GetTopRatedMoviesEvent>((event, emit) async {});
   }
 }
