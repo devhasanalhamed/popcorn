1f5fba3346181760e255d38fc2f447c74b9acf85
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-10 16:27:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PopCorn',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
