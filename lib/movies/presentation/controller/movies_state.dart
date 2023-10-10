ae02ffd3de2a2772e605932f60287fb642d31c7e
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-10 10:35:57 +0300

    Update movies_state.dart
    adding to the state of the movie bloc

--- lib/movies/presentation/controller/movies_state.dart
+++ lib/movies/presentation/controller/movies_state.dart
@@ -6,11 +6,23 @@ class MoviesState extends Equatable {
   final List<MovieModel> nowPlayingMovies;
   final RequestState nowPlayingMoviesState;
   final String nowPlayingMessage;
+  final List<MovieModel> popularMovies;
+  final RequestState popularMoviesState;
+  final String popularMessage;
+  final List<MovieModel> topRatedMovies;
+  final RequestState topRatedMoviesState;
+  final String topRatedMessage;
 
   const MoviesState({
     this.nowPlayingMovies = const [],
     this.nowPlayingMoviesState = RequestState.loading,
     this.nowPlayingMessage = '',
+    this.popularMovies = const [],
+    this.popularMoviesState = RequestState.loading,
+    this.popularMessage = '',
+    this.topRatedMovies = const [],
+    this.topRatedMoviesState = RequestState.loading,
+    this.topRatedMessage = '',
   });
 
   @override
