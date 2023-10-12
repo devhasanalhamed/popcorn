494379112fcd037197d8b87e5645a7bdc0797c04
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-12 13:28:18 +0300

    Update get_now_playing_movies_usecase.dart

--- lib/movies/domain/usecase/get_now_playing_movies_usecase.dart
+++ lib/movies/domain/usecase/get_now_playing_movies_usecase.dart
@@ -1,16 +1,18 @@
 import 'package:dartz/dartz.dart';
 import 'package:popcorn/core/error/failure.dart';
+import 'package:popcorn/core/usecase/base_usecase.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
 import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
 
-class GetNowPlayingMoviesUseCase {
+class GetNowPlayingMoviesUseCase extends UseCase<List<MovieModel>> {
   final BaseMoviesRepository baseMoviesRepository;
 
   GetNowPlayingMoviesUseCase({
     required this.baseMoviesRepository,
   });
 
-  Future<Either<Failure, List<MovieModel>>> execute() async {
+  @override
+  Future<Either<Failure, List<MovieModel>>> call() async {
     return await baseMoviesRepository.getNowPlayingMovies();
   }
 }
