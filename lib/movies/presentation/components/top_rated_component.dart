674bf0dc2c7d60b21a83afb9699908d8f6200465
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-12 13:28:30 +0300

    Update top_rated_component.dart

--- lib/movies/presentation/components/top_rated_component.dart
+++ lib/movies/presentation/components/top_rated_component.dart
@@ -1,11 +1,10 @@
-import 'dart:developer';
-
 import 'package:animate_do/animate_do.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:popcorn/core/network/api_constants.dart';
 import 'package:popcorn/core/services/size_config.dart';
+import 'package:popcorn/core/utils/enums.dart';
 import 'package:popcorn/movies/presentation/controller/movies_bloc.dart';
 import 'package:popcorn/movies/presentation/controller/movies_state.dart';
 import 'package:shimmer/shimmer.dart';
@@ -16,53 +15,64 @@ class TopRatedComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return BlocBuilder<MoviesBloc, MoviesState>(builder: (context, state) {
-      log('build');
-      return FadeIn(
-        duration: const Duration(milliseconds: 500),
-        child: SizedBox(
-          height: SizeConfig.safeBlockVertical! * 20,
-          child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            itemCount: state.topRatedMovies.length,
-            itemBuilder: (context, index) {
-              final movie = state.topRatedMovies[index];
-              return Container(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: InkWell(
-                  onTap: () {
-                    /// TODO : NAVIGATE TO  MOVIE DETAILS
-                  },
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                    child: CachedNetworkImage(
-                      width: 120.0,
-                      fit: BoxFit.cover,
-                      imageUrl: ApiConstants.imageUrl(movie.backdropPath),
-                      placeholder: (context, url) => Shimmer.fromColors(
-                        baseColor: Colors.grey[850]!,
-                        highlightColor: Colors.grey[800]!,
-                        child: Container(
-                          height: SizeConfig.safeBlockVertical! * 20,
-                          width: 120.0,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(8.0),
+    return BlocBuilder<MoviesBloc, MoviesState>(
+      buildWhen: (previous, current) =>
+          previous.topRatedMoviesState != current.topRatedMoviesState,
+      builder: (context, state) {
+        switch (state.topRatedMoviesState) {
+          case RequestState.loading:
+            return const Text('loading');
+          case RequestState.loaded:
+            return FadeIn(
+              duration: const Duration(milliseconds: 500),
+              child: SizedBox(
+                height: SizeConfig.safeBlockVertical! * 20,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  itemCount: state.topRatedMovies.length,
+                  itemBuilder: (context, index) {
+                    final movie = state.topRatedMovies[index];
+                    return Container(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: InkWell(
+                        onTap: () {
+                          /// TODO : NAVIGATE TO  MOVIE DETAILS
+                        },
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8.0)),
+                          child: CachedNetworkImage(
+                            width: 120.0,
+                            fit: BoxFit.cover,
+                            imageUrl: ApiConstants.imageUrl(movie.backdropPath),
+                            placeholder: (context, url) => Shimmer.fromColors(
+                              baseColor: Colors.grey[850]!,
+                              highlightColor: Colors.grey[800]!,
+                              child: Container(
+                                height: SizeConfig.safeBlockVertical! * 20,
+                                width: 120.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
                           ),
                         ),
                       ),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
-                  ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
-        ),
-      );
-    });
+              ),
+            );
+          case RequestState.error:
+            return const Text('error');
+        }
+      },
+    );
   }
 }
