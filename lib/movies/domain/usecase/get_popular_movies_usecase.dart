f902da042d376c649e8ac41848cb895f22abbe33
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-12 13:28:20 +0300

    Update get_popular_movies_usecase.dart

--- lib/movies/domain/usecase/get_popular_movies_usecase.dart
+++ lib/movies/domain/usecase/get_popular_movies_usecase.dart
@@ -1,16 +1,18 @@
 import 'package:dartz/dartz.dart';
 import 'package:popcorn/core/error/failure.dart';
+import 'package:popcorn/core/usecase/base_usecase.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
 import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
 
-class GetPopularMoviesUseCase {
+class GetPopularMoviesUseCase extends UseCase<List<MovieModel>> {
   final BaseMoviesRepository baseMoviesRepository;
 
   GetPopularMoviesUseCase({
     required this.baseMoviesRepository,
   });
 
-  Future<Either<Failure, List<MovieModel>>> execute() async {
+  @override
+  Future<Either<Failure, List<MovieModel>>> call() async {
     return await baseMoviesRepository.getPopularMovies();
   }
 }
