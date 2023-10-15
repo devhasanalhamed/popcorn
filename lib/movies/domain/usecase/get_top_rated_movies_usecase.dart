46f079ddf386a76bb951d09c53919e7edb52d09b
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 14:17:32 +0300

    Update get_top_rated_movies_usecase.dart

--- lib/movies/domain/usecase/get_top_rated_movies_usecase.dart
+++ lib/movies/domain/usecase/get_top_rated_movies_usecase.dart
@@ -4,14 +4,14 @@ import 'package:popcorn/core/usecase/base_usecase.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
 import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
 
-class GetTopRatedMoviesUseCase extends BaseUseCase<List<MovieModel>> {
+class GetTopRatedMoviesUseCase extends BaseUseCase<List<MovieModel>, NoParameters> {
   final BaseMoviesRepository baseMoviesRepository;
 
   GetTopRatedMoviesUseCase({
     required this.baseMoviesRepository,
   });
   @override
-  Future<Either<Failure, List<MovieModel>>> call() async {
+  Future<Either<Failure, List<MovieModel>>> call(NoParameters parameters) async {
     return await baseMoviesRepository.getTopRatedMovies();
   }
 }
