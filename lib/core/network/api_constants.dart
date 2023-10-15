02a3d8dd7e922b4f7e492a2ee8fe35ba25ed3ba2
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 14:14:41 +0300

    Update api_constants.dart
    rename URL to Path and add new path for movie details

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,10 +1,11 @@
 class ApiConstants {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String basePath = 'https://api.themoviedb.org/3';
   static const String apiKey = 'api_key=944496b8feb259fafe467ef6f4d38c80';
-  static const String movieUrl = '$baseUrl/movie';
-  static const String nowPlayingMoviesUrl = '$movieUrl/now_playing?$apiKey';
-  static const String popularMoviesUrl = '$movieUrl/popular?$apiKey';
-  static const String topRatedMoviesUrl = '$movieUrl/top_rated?$apiKey';
+  static const String moviePath = '$basePath/movie';
+  static const String nowPlayingMoviesPath = '$moviePath/now_playing?$apiKey';
+  static const String popularMoviesPath = '$moviePath/popular?$apiKey';
+  static const String topRatedMoviesPath = '$moviePath/top_rated?$apiKey';
+  static String movieDetailPath(int movieId) => '$moviePath/$movieId?$apiKey';
   static const String baseImageUrl = 'https://image.tmdb.org/t/p/original';
   static String imageUrl(String path) => '$baseImageUrl$path';
 }
