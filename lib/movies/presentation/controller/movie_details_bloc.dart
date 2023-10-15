161e0187073f91d4be20bbe9192318971de7d8bc
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 23:29:11 +0300

    Create movie_details_bloc.dart

---
+++ lib/movies/presentation/controller/movie_details_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:popcorn/core/utils/enums.dart';
+import 'package:popcorn/movies/domain/entity/movie_details.dart';
+import 'package:popcorn/movies/domain/usecase/get_movie_details_usecase.dart';
+
+part 'movie_details_event.dart';
+part 'movie_details_state.dart';
+
+class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
+  MovieDetailsBloc(this.getMovieDetailsUseCase)
+      : super(const MovieDetailsState()) {
+    on<GetMovieDetailsEvent>(_getMovieDetails);
+  }
+
+  final GetMovieDetailsUseCase getMovieDetailsUseCase;
+
+  FutureOr<void> _getMovieDetails(
+      GetMovieDetailsEvent event, Emitter<MovieDetailsState> emit) async {
+    final result =
+        await getMovieDetailsUseCase(MovieDetailsParameters(movieId: event.id));
+
+    result.fold(
+      (l) => emit(state.copyWith(
+        movieDetailsState: RequestState.error,
+        movieDetailsMessage: l.message,
+      )),
+      (r) => emit(state.copyWith(
+        movieDetails: r,
+        movieDetailsState: RequestState.loaded,
+      )),
+    );
+  }
+}
