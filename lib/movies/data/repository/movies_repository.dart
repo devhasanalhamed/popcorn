ce1675c456ae2c762bb14e790ab1fde85f5e8b47
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 23:29:01 +0300

    Update movies_repository.dart

--- lib/movies/data/repository/movies_repository.dart
+++ lib/movies/data/repository/movies_repository.dart
@@ -4,7 +4,9 @@ import 'package:popcorn/core/error/failure.dart';
 import 'package:popcorn/movies/data/datasource/movie_remote_data_source.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
 import 'package:popcorn/movies/domain/entity/movie_details.dart';
+import 'package:popcorn/movies/domain/entity/recomendations.dart';
 import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
+import 'package:popcorn/movies/domain/usecase/get_movie_details_usecase.dart';
 
 class MoviesRepository extends BaseMoviesRepository {
   final BaseMoviesRemoteDataSource baseMovieRemoteDataSource;
@@ -47,8 +49,20 @@ class MoviesRepository extends BaseMoviesRepository {
   }
 
   @override
-  Future<Either<Failure, MovieDetails>> getMovieDetails() {
-    // TODO: implement getMovieDetails
+  Future<Either<Failure, MovieDetails>> getMovieDetails(
+      MovieDetailsParameters parameters) async {
+    final result = await baseMovieRemoteDataSource.getMovieDetails(parameters);
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(
+          ServerFailure(message: failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Recomendations>>> getRecomendations() {
+    // TODO: implement getRecomendations
     throw UnimplementedError();
   }
 }
