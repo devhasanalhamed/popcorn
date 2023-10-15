590f23fee4fcbf5741dc3c33e30b8f8548d0bb9d
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 14:17:13 +0300

    Update get_movie_details_usecase.dart
    create the class for the get movie details use case and the class for the parameters

--- lib/movies/domain/usecase/get_movie_details_usecase.dart
+++ lib/movies/domain/usecase/get_movie_details_usecase.dart
@@ -1,11 +1,31 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:popcorn/core/error/failure.dart';
 import 'package:popcorn/core/usecase/base_usecase.dart';
 import 'package:popcorn/movies/domain/entity/movie_details.dart';
+import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
 
-class GetMovieDetails extends BaseUseCase<MovieDetails> {
+class GetMovieDetailsUseCase
+    extends BaseUseCase<MovieDetails, MovieDetailsParameters> {
+  final BaseMoviesRepository baseMoviesRepository;
+
+  GetMovieDetailsUseCase({
+    required this.baseMoviesRepository,
+  });
+
   @override
-  Future<Either<Failure, MovieDetails>> call() {
-    throw UnimplementedError();
+  Future<Either<Failure, MovieDetails>> call(
+      MovieDetailsParameters parameters) async {
+    return await baseMoviesRepository.getMovieDetails();
   }
+}
+
+class MovieDetailsParameters extends Equatable {
+  final int movieId;
+  const MovieDetailsParameters({
+    required this.movieId,
+  });
+
+  @override
+  List<Object?> get props => [movieId];
 }
