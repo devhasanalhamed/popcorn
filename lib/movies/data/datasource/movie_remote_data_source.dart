869086b836bc3b533e9801f7b78d758f0ec4e19a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 23:28:57 +0300

    Update movie_remote_data_source.dart

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -2,7 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:popcorn/core/error/exceptions.dart';
 import 'package:popcorn/core/network/api_constants.dart';
 import 'package:popcorn/core/network/error_message_model.dart';
+import 'package:popcorn/movies/data/models/movie_details_model.dart';
 import 'package:popcorn/movies/data/models/movie_model.dart';
+import 'package:popcorn/movies/domain/usecase/get_movie_details_usecase.dart';
 
 abstract class BaseMoviesRemoteDataSource {
   Future<List<MovieModel>> getNowPlayingMovies(); //? this is called contract
@@ -10,6 +12,8 @@ abstract class BaseMoviesRemoteDataSource {
   Future<List<MovieModel>> getPopularMovies();
 
   Future<List<MovieModel>> getTopRatedMovies();
+
+  Future<MovieDetailsModel> getMovieDetails(MovieDetailsParameters parameters);
 }
 
 class MoviesRemoteDataSource extends BaseMoviesRemoteDataSource {
@@ -43,6 +47,19 @@ class MoviesRemoteDataSource extends BaseMoviesRemoteDataSource {
     if (response.statusCode == 200) {
       return List<MovieModel>.from((response.data['results'] as List)
           .map((e) => MovieModel.fromJson(e)));
+    } else {
+      throw ServerException(
+          errorMessageModel: ErrorMessageModel.fromJson(response.data));
+    }
+  }
+
+  @override
+  Future<MovieDetailsModel> getMovieDetails(
+      MovieDetailsParameters parameters) async {
+    final response =
+        await Dio().get(ApiConstants.movieDetailPath(parameters.movieId));
+    if (response.statusCode == 200) {
+      return MovieDetailsModel.fromJson(response.data);
     } else {
       throw ServerException(
           errorMessageModel: ErrorMessageModel.fromJson(response.data));
