4142d4aea937eb49bcb59ba28c6f6a38544f3709
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 14:15:43 +0300

    Create genres_model.dart
    create genres model in the data models

---
+++ lib/movies/data/models/genres_model.dart
@@ -0,0 +1,13 @@
+import 'package:popcorn/movies/domain/entity/geners.dart';
+
+class GenresModel extends Genres {
+  const GenresModel({
+    required super.id,
+    required super.name,
+  });
+
+  factory GenresModel.fromJson(Map<String, dynamic> json) => GenresModel(
+        id: json['id'],
+        name: json['name'],
+      );
+}
