fe2fed31feb69de6bf01f9d020c358c5fed8096a
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-15 23:28:54 +0300

    Update services_locator.dart
    add the models to the locator

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -2,9 +2,11 @@ import 'package:get_it/get_it.dart';
 import 'package:popcorn/movies/data/datasource/movie_remote_data_source.dart';
 import 'package:popcorn/movies/data/repository/movies_repository.dart';
 import 'package:popcorn/movies/domain/repository/base_movies_repository.dart';
+import 'package:popcorn/movies/domain/usecase/get_movie_details_usecase.dart';
 import 'package:popcorn/movies/domain/usecase/get_now_playing_movies_usecase.dart';
 import 'package:popcorn/movies/domain/usecase/get_popular_movies_usecase.dart';
 import 'package:popcorn/movies/domain/usecase/get_top_rated_movies_usecase.dart';
+import 'package:popcorn/movies/presentation/controller/movie_details_bloc.dart';
 import 'package:popcorn/movies/presentation/controller/movies_bloc.dart';
 
 final sl = GetIt.instance;
@@ -13,6 +15,7 @@ class ServicesLocator {
   void init() {
     ///BLOC
     sl.registerFactory(() => MoviesBloc(sl(), sl(), sl()));
+    sl.registerFactory(() => MovieDetailsBloc(sl()));
 
     ///USE CASE
     sl.registerLazySingleton(
@@ -21,6 +24,8 @@ class ServicesLocator {
         () => GetPopularMoviesUseCase(baseMoviesRepository: sl()));
     sl.registerLazySingleton(
         () => GetTopRatedMoviesUseCase(baseMoviesRepository: sl()));
+    sl.registerLazySingleton(
+        () => GetMovieDetailsUseCase(baseMoviesRepository: sl()));
 
     ///Repository
     sl.registerLazySingleton<BaseMoviesRepository>(
