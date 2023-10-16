faddfa74f06725cf67c101e4d3ed89ed272f8628
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-16 20:06:59 +0300

    Create recommendations.dart

---
+++ lib/movies/domain/entity/recommendations.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class Recommendations extends Equatable {
+  final int id;
+  final String backdropPath;
+
+  const Recommendations({
+    required this.id,
+    required this.backdropPath,
+  });
+
+  @override
+  List<Object> get props => [id, backdropPath];
+}
