2e35c451d39b1ad052c8120e7996a3321ed5d203
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-17 09:57:22 +0300

    Update movie_details_bloc.dart
    use flutter bloc instead bloc

--- lib/movies/presentation/controller/movie_details_bloc.dart
+++ lib/movies/presentation/controller/movie_details_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:popcorn/core/utils/enums.dart';
 import 'package:popcorn/movies/domain/entity/movie_details.dart';
 import 'package:popcorn/movies/domain/entity/recommendations.dart';
