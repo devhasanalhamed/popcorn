2fc9cddda633aa256b7d13a7dc849fdfea9f394f
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-17 10:18:48 +0300

    Create app_strings.dart
    this file will contain all the application strings to easy maintaining

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  static const appTitle = 'popcorn';
+  static const loading = 'Loading';
+  static const popular = 'Popular';
+  static const nowPlaying = 'NOW PLAYING';
+  static const topRated = 'Top Rated';
+  static const seeMore = 'See More';
+  static const genres = 'Genres';
+  static const moreLikeThis = 'MORE LIKE THIS';
+}
