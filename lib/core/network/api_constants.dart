cf21ea1340661c110126581e00d52d5a7d647aa7
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-19 07:18:09 +0300

    Update api_constants.dart

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,16 +1,20 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
   ///Constants
-  static const String basePath = 'https://api.themoviedb.org/3';
-  static const String apiKey = 'api_key=944496b8feb259fafe467ef6f4d38c80';
-  static const String moviePath = '$basePath/movie';
-  static const String nowPlayingMoviesPath = '$moviePath/now_playing?$apiKey';
-  static const String popularMoviesPath = '$moviePath/popular?$apiKey';
-  static const String topRatedMoviesPath = '$moviePath/top_rated?$apiKey';
-  static const String baseImageUrl = 'https://image.tmdb.org/t/p/original';
+  static String basePath = dotenv.env['BASE_URL']!;
+  static String apiKey = dotenv.env['API_KEY']!;
+  static String moviePath = '$basePath/movie';
+  static String nowPlayingMoviesPath = '$moviePath/now_playing?$apiKey';
+  static String popularMoviesPath = '$moviePath/popular?$apiKey';
+  static String topRatedMoviesPath = '$moviePath/top_rated?$apiKey';
+  static String baseImageUrl = dotenv.env['BASE_IMAGE_URL']!;
 
   ///Functions
   static String movieDetailPath(int movieId) => '$moviePath/$movieId?$apiKey';
+
   static String recommendationsPath(int movieId) =>
       '$moviePath/$movieId/recommendations?$apiKey';
+
   static String imageUrl(String path) => '$baseImageUrl$path';
 }
