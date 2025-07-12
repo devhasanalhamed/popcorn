a74a2a0583a8e089fb47f5221523b1e01a637f76
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-07-12 18:15:27 +0300

    Update theme_state.dart

--- lib/core/global/theme/controller/theme_state.dart
+++ lib/core/global/theme/controller/theme_state.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +5,7 @@ class ThemeState extends Equatable {
   final bool isDark;
   final ThemeData themeData;
 
-  const ThemeState({this.isDark = false, this.themeData = const ThemeData()});
+  const ThemeState({this.isDark = false, required this.themeData});
 
   @override
   List<Object> get props => [isDark];
